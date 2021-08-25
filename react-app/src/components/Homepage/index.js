5f8eff900d9c69e5e21e780b1853c49f4bdd0705
michellekontoff <mlkontoff@gmail.com>
2021-08-25 19:40:28 -0400

    finished styling landing page

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
 import LoginFormModal from '../auth/LoginFormModal'
-import SignUpFormMoal from '../auth/SignUpFormModal'
+import SignUpFormModal from '../auth/SignUpFormModal'
 import welcome from '../../images/welcome_v2.png'
 import about from '../../images/about.jpg'
 import './homepage.css'
@@ -22,14 +22,14 @@ export default function Homepage() {
                 </div>
                 <div className='header__buttons'>
                     <LoginFormModal />
-                    <SignUpFormMoal />
+                    <SignUpFormModal />
                 </div>
             </div>
             <div className='about'>
                 <div className='about__message'>
                     Babeling Book is a blogging site for people who want to practice writing about the things they care about in a language they want to learn. You can make posts and leave comments. The difference between Babeling Book and other blogging sites is here users are <i>encouraged</i> to offer each other corrections to grammar and spelling. Verified members of the community (coming soon), can even make corrections through annotations on a post, making their notes easy read side-by-side with the post, and viewable to other users who want to learn from your post.
 
-                    <p>Log in or sign up to see more!</p>
+                    <p><LoginFormModal /> or <SignUpFormModal /> to see more!</p>
                 </div>
                 <div className='about__img-container'>
                     <img className='about__img' src={about} alt='img'></img>
