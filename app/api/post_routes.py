4a1b6494656fa8b84f4942928d4fbe8dcb1fd20f
michellekontoff <mlkontoff@gmail.com>
2021-08-26 16:49:05 -0400

    edit form + route works. delete route works

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,10 +1,48 @@
 from flask import Blueprint, request
-from app.forms import PostCreateForm
+from app.forms import PostCreateForm, PostEditForm
 from app.models import Post, db
+from datetime import datetime
+
 
 bp = Blueprint('posts', __name__)
 
-@bp.route('/new', methods=['POST'])
+# GET ALL POSTS
+@bp.route('/')
+def get_all_posts():
+    posts = Post.query.all()
+
+    return { post.to_dict()['id']: post.to_dict() for post in posts }
+
+# GET, EDIT, DELETE POST BY ID
+@bp.route('/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def get_post_by_id(id):
+    post = Post.query.get(id)
+
+    if post is None:
+        return {'error': 'Post could not be found.'}
+
+    if request.method == 'GET':
+        return post.to_dict()
+
+    elif request.method == 'PUT':
+        form = PostEditForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            form.populate_obj(post)
+            post.updated_at = datetime.now()
+            db.session.add(post)
+            db.session.commit()
+            return post.to_dict()
+        else:
+            return form.errors
+            
+    elif request.method == 'DELETE':
+        db.session.delete(post)
+        db.session.commit()
+        return { "deletion": "successful" }
+
+# CREATE POST
+@bp.route('/', methods=['POST'])
 def create_post():
     form = PostCreateForm()
     form['csrf_token'].data = request.cookies['csrf_token']
---
+++ app/forms/post_edit_form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+
+class PostEditForm(FlaskForm):
+    title = StringField('title', validators=[Length(max=50, message='Title must be fewer than 50 characters.')])
+    content = TextAreaField('content', validators=[DataRequired(message='This field cannot be empty.')])
+    language_id = IntegerField('language_id')
+    user_id = IntegerField('user_id')
