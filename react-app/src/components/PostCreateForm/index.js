9aefd24d2d9cb5bb6eb572f69f6bba4375fb7eb4
michellekontoff <mlkontoff@gmail.com>
2021-08-26 20:03:32 -0400

    styled create post form. is create done???

--- react-app/src/components/PostCreateForm/index.js
+++ react-app/src/components/PostCreateForm/index.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 
+import image from '../../images/postCreate.jpg'
+
+import './postCreateForm.css'
+
 export default function PostCreateForm(){
     const history = useHistory()
 
@@ -18,9 +22,7 @@ export default function PostCreateForm(){
 
         if (res.ok) {
             const list = await res.json()
-            console.log(list)
             setLanguageList(list.languages)
-            
 
         } else {
             return 'Something went wrong.'
@@ -61,31 +63,35 @@ export default function PostCreateForm(){
 
 
     return (
-        <form className='post-create-form post-form' onSubmit={(e) => submitPost(e)}>
-            <label>Title</label>
-            <input
-                type='text'
-                name='title'
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            
-            <label>Content</label>
-            {/* { errors?.length > 0 ? <p className='errors'>{errors.content}</p> : <p>hi</p>} */}
-            <label className='errors'>{errors?.content}</label>
-            <textarea
-                name='content'
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-            ></textarea>
-            
-            <label>Language</label>
-            <select onChange={(e) => setLanguageId(e.target.value)}>
-                {languageList?.map((language) => {
-                   return <option key={language.id} value={language.id}>{language.name}</option>
-                })}
-            </select>
-            <button type='submit'>Create post</button>
-        </form>
+        <div className='post-create-container'>
+            <img id='post-create-img' src={image}></img>
+            <form className='post-create-form post-form' onSubmit={(e) => submitPost(e)}>
+                {/* <h2>Create Post</h2> */}
+                <label>Title</label>
+                <input
+                    type='text'
+                    name='title'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                
+                <label>Content</label>
+                <label className='errors'>{errors?.content}</label>
+                <textarea
+                    name='content'
+                    rows='22'
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                
+                <label className='language-label'>Language</label>
+                <select onChange={(e) => setLanguageId(e.target.value)}>
+                    {languageList?.map((language) => {
+                    return <option key={language.id} value={language.id}>{language.name}</option>
+                    })}
+                </select>
+                <button type='submit'>Create New Post</button>
+            </form>
+        </div>
     )
 }
