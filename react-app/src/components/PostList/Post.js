ed11547a1e58faebe73cdc15e4125df0e5560403
michellekontoff <mlkontoff@gmail.com>
2021-08-27 22:49:01 -0400

    post list only shows 1st 200 chars of each post

--- react-app/src/components/PostList/Post.js
+++ react-app/src/components/PostList/Post.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function PostList({ post }) {
-    // useEffects
+    const [subString, setSubString] = useState(false)
+
+    useEffect(() => {
+
+        if (post.content.length > 200) {
+            setSubString(post.content.substring(0, 200) + ' ...')
+        }
+        
+    }, [post.content])
+
     return (
         <>
             <Link to={`/posts/${post.id}`}><div className='post__title'>
@@ -12,7 +21,7 @@ export default function PostList({ post }) {
                 <Link to={`/users/${post.owner?.id}`}>{post.owner?.username}</Link> : { post.language?.name } - {post.created_at}
             </div>
             <Link to={`/posts/${post.id}`}><div className='post__content'>
-                {post.content}
+                {post.content?.length < 200 ? <>{post.content}</> : <>{subString}</>}
             </div></Link>
         </>
     )
