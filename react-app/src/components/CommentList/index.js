2b6b348d46c6784f5782d6b14d105e0236ee431a
michellekontoff <mlkontoff@gmail.com>
2021-08-30 14:36:30 -0400

    added partial border between comments

--- react-app/src/components/CommentList/index.js
+++ react-app/src/components/CommentList/index.js
@@ -1,41 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import Comment from './Comment'
-import { useParams } from 'react-router'
+import React, { useState, useEffect } from "react";
+import Comment from "./Comment";
+import { useParams } from "react-router";
 
-import './commentList.css'
+import "./commentList.css";
 
-export default function CommentList({ postOwnerId, addComment, userId}) {
-    const params = useParams()
-    const [comments, setComments] = useState([])
-    const [comment, setComment] = useState({})
+export default function CommentList({ postOwnerId, addComment, userId }) {
+   const params = useParams();
+   const [comments, setComments] = useState([]);
+   const [comment, setComment] = useState({});
 
-    async function getPostComments(id){
-        const res = await fetch(`/api/posts/${id}/comments`)
+   async function getPostComments(id) {
+      const res = await fetch(`/api/posts/${id}/comments`);
 
-        if (res.ok) {
-            const data = await res.json()
-            setComments(data.comments)
-        } else {
-            return 'Something went wrong.'
-        }
-    }
+      if (res.ok) {
+         const data = await res.json();
+         setComments(data.comments);
+      } else {
+         return "Something went wrong.";
+      }
+   }
 
-    useEffect(() => {
-        getPostComments(params.postId)
+   useEffect(() => {
+      getPostComments(params.postId);
+   }, [comment, addComment, params.postId]);
 
-    }, [comment, addComment, params.postId])
-    
-    return (
-        <>
-            { comments?.length ? 
-            <div className='comment-list'>
-                {comments?.map((comment, i) => {
-                    return <div key={comment.id} className='comment'>
-                        <Comment commentId={comment.id} postOwnerId={postOwnerId} userId={userId} comment={comment} setComment={setComment}/>
-                    </div>
-                })}
-            </div> 
-            : null}
-        </>
-    )
+   return (
+      <>
+         {comments?.length ? (
+            <div className="comment-list">
+               {comments?.map((comment, i) => {
+                  return (
+                     <>
+                        <div key={comment.id} className="comment">
+                           <Comment
+                              commentId={comment.id}
+                              postOwnerId={postOwnerId}
+                              userId={userId}
+                              comment={comment}
+                              setComment={setComment}
+                           />
+                           {comments[i + 1] ?
+                           <div className='comment__border'></div>
+                           : null}
+                        </div>
+
+                     </>
+                  );
+               })}
+            </div>
+         ) : null}
+      </>
+   );
 }
