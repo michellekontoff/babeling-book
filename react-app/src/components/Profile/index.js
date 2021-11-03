432198f0806f2aa34381f90cc70a6b08d3b1139d
michellekontoff <mlkontoff@gmail.com>
2021-11-03 19:04:02 -0400

    fixed empty list msg if profile doesnt belong to current user

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import PostList from "../PostList";
@@ -6,19 +7,24 @@ import PostList from "../PostList";
 import "./profile.css";
 
 export default function Profile() {
+   const currentUser = useSelector((state) => state.session.user)
    const [user, setUser] = useState({});
    const [posts, setPosts] = useState([]);
+   const [userId, setUserId] = useState(null)
    const params = useParams();
 
    async function getPosts(userId) {
       const res = await fetch(`/api/users/${userId}/posts`);
-
       if (res.ok) {
          const data = await res.json();
          if (data.posts.length) {
             setPosts(data.posts);
          } else {
-             setPosts((<p align="center">You don't have any posts yet. Why don't you try <Link to='/posts/new' className='first-post'>writing one</Link>?</p>))
+             if (currentUser.id === userId) {
+                 setPosts((<p align="center">You don't have any posts yet. Why don't you try <Link to='/posts/new' className='first-post'>writing one</Link>?</p>))
+             } else {
+                 setPosts((<p align="center">This user hasn't made any posts yet.</p>))
+             }
          }
       } else {
          return "Something went wrong.";
@@ -37,7 +43,8 @@ export default function Profile() {
    }
 
    useEffect(() => {
-      const userId = params.userId;
+      const userId = parseInt(params.userId);
+      setUserId(userId)
       getUser(userId);
       getPosts(userId);
    }, [params.userId]);
@@ -45,7 +52,7 @@ export default function Profile() {
    return (
       <>
          <div className="posts-latest content">
-            <h1>{user.username}'s Posts</h1>
+            <h1>{user.username && `${user.username}'s Posts`}</h1>
             {posts.length > 0 ? <PostList posts={posts} /> : <>{posts}</> }
          </div>
       </>
