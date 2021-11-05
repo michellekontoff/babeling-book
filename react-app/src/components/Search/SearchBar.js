b1ebc6949613bffbe0a8fe77c2498804dc4c8428
michellekontoff <mlkontoff@gmail.com>
2021-11-04 22:16:39 -0400

    searchbar form works now

--- react-app/src/components/Search/SearchBar.js
+++ react-app/src/components/Search/SearchBar.js
@@ -1,29 +1,21 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function SearchBar () {
     const [search, setSearch] = useState('')
-    // const [error, setError]
 
     const history = useHistory()
 
-
-    /* have a controlled input for search term.
-        upon hitting search button, perform search fx.
-        search fx checks length of term
-            if < 40 chars, then make fetch reqs to both users and posts
-            if > 40, only fetch to posts
-        in backend: users checks usernames
-                    posts checks matches in title and content
-                    if post matches both title and content, only return it once
-    */
+    async function submitSearch(e) {
+        e.preventDefault()
+        history.push(`/search?q=${search}`)
+    }
 
     return (
         <form className='search'
-        onSubmit={() => history.push(`/search?q=${search}`)}
+        onSubmit={submitSearch}
         >
             <input type='text'
             className='search__input'
@@ -31,13 +23,11 @@ export default function SearchBar () {
             placeholder='Search...'
             onChange={(e) => setSearch(e.target.value)}
             />
-            {/* <Link to={`/search/${search}`}> */}
-                <button type='submit'
-                className='search__btn'
-                >
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
-            {/* </Link> */}
+            <button type='submit'
+            className='search__btn'
+            >
+                <FontAwesomeIcon icon={faSearch} />
+            </button>
         </form>
     )
 }
