e9c7a40af38469f8f8f640079feecbb65f1d3271
michellekontoff <mlkontoff@gmail.com>
2021-11-07 14:38:21 -0500

    fixed length of querymatch + substring. content highlight finished

--- react-app/src/components/PostList/Post.js
+++ react-app/src/components/PostList/Post.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { highlightQuery } from "../Search/utils";
 
 export default function Post({ post }) {
    const [subString, setSubString] = useState(false);
@@ -7,76 +8,19 @@ export default function Post({ post }) {
 
    const query = new URLSearchParams(search).get('q');
 
-   function highlightQuery(query) {
-        // const title = post.title
-        const content = post.content
-        //content match
-       if (query) {
-            const queryIndex = content.toLowerCase().indexOf(query.toLowerCase())
-
-            let queryMatch;
-            let beforeQuery;
-            let afterQuery;
-            let matchFound;
-
-            if (content.length < 300 && query.length < 300) {
-
-                queryMatch = content.slice(queryIndex, queryIndex + query.length)
-                beforeQuery = content.slice(0, queryIndex)
-                afterQuery = content.slice(queryIndex + query.length)
-
-            } else if (content.length > 300 && query.length < 300) {
-
-                queryMatch = content.slice(queryIndex, queryIndex + query.length)
-
-                beforeQuery = queryIndex - 100 > 0
-                              ? '... ' +  content.slice(queryIndex - 100, queryIndex)
-                              : content.slice(0, queryIndex)
-
-                afterQuery = queryIndex + query.length + 100 < content.length
-                            ? content.slice(queryIndex + query.length, (queryIndex + query.length + 100)) + ' ...'
-                            : content.slice(queryIndex + query.length)
-
-            } else if (query.length > 300) {
-                matchFound = (<span className="query-match">{queryMatch}</span>)
-
-                setSubString(matchFound)
-                return
-            }
-
-            matchFound = (
-                <>
-                    {beforeQuery}
-                    <span className="query-match">{queryMatch}</span>
-                    {afterQuery}
-                </>
-            )
-            
-            setSubString(matchFound)
-       }
-
-       //title match
-   }
-
-   /* 
-        check if urlsearchparams exist
-        if they do, index search match
-        then set substring start before index and end after index
-        highlight search term if possible
-        need to account for query length - if it's too long then just set the query as substring
-   */
-
    useEffect(() => {
-      highlightQuery(query);
-      if (post.content.length > 200) {
+       if (query) {
+           highlightQuery(query, post, setSubString);
+           return;
+       } else if (!query && post.content.length > 200) {
          setSubString(post.content.substring(0, 200) + " ...");
       }
-   }, [post.content, query]);
+   }, [post, post.content, query]);
 
-   useEffect(() => {
-       console.log(subString)
-       console.log(query)
-   })
+//    useEffect(() => {
+//        console.log(subString)
+//        console.log(query)
+//    })
 
    return (
       <>
--- react-app/src/components/Search/utils.js
+++ react-app/src/components/Search/utils.js
@@ -19,3 +19,54 @@ export const searchPosts = async (search, setPosts) => {
     }
     return;
 }
+
+export function highlightQuery(query, post, setSubString) {
+    // const title = post.title
+    const content = post.content
+    //content match
+   if (query) {
+        const queryIndex = content.toLowerCase().indexOf(query.toLowerCase())
+
+        let queryMatch;
+        let beforeQuery;
+        let afterQuery;
+        let matchFound;
+
+        if (content.length < 300 && query.length < 300) {
+
+            queryMatch = content.slice(queryIndex, queryIndex + query.length)
+            beforeQuery = content.slice(0, queryIndex)
+            afterQuery = content.slice(queryIndex + query.length)
+
+        } else if (content.length > 300 && query.length < 300) {
+
+            queryMatch = content.slice(queryIndex, queryIndex + query.length)
+
+            beforeQuery = queryIndex - 200 > 0
+                          ? '... ' +  content.slice(queryIndex - 200, queryIndex)
+                          : content.slice(0, queryIndex)
+
+            afterQuery = queryIndex + query.length + 200 < content.length
+                        ? content.slice(queryIndex + query.length, (queryIndex + query.length + 200)) + ' ...'
+                        : content.slice(queryIndex + query.length)
+
+        } else if (query.length > 300) {
+            matchFound = (<span className="query-match">{queryMatch}</span>)
+
+            setSubString(matchFound)
+            return
+        }
+
+        matchFound = (
+            <>
+                {beforeQuery}
+                <span className="query-match">{queryMatch}</span>
+                {afterQuery}
+            </>
+        )
+        
+        setSubString(matchFound)
+   }
+
+   //title match
+}
