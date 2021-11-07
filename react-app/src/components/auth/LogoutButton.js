a54dac6d00274c66579d97d384312a175f8e8d32
michellekontoff <mlkontoff@gmail.com>
2021-11-07 15:24:33 -0500

    removed unused destructured props from logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 
-const LogoutButton = ({showNav, setShowNav}) => {
+const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
