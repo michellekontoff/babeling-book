ba71e756436e89ee1f6ef7c87a77ede5924cef8e
michellekontoff <mlkontoff@gmail.com>
2021-11-26 16:53:28 -0500

    built nexpagelinks fx

--- react-app/src/components/postUtils.js
+++ react-app/src/components/postUtils.js
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom"
+
 export async function getLanguages(cb) {
     const res = await fetch('/api/languages')
 
@@ -8,4 +10,20 @@ export async function getLanguages(cb) {
     } else {
         return 'Something went wrong.'
     }
+}
+
+export function nextPageLinks(nextPages, query) {
+    let links = (
+        <nav className="page-links">
+            {nextPages.map((pageNum, i) => {
+            <NavLink
+                to={`/search?q=${query}&page=${pageNum}`}
+                key={i}
+                activeClassName="active-page"
+                >{pageNum}</NavLink>
+            })}
+        </nav>
+    );
+
+    return links;
 }
