24a0cc72be8f29979b2f7901eaa50a1284f0be04
michellekontoff <mlkontoff@gmail.com>
2021-11-26 15:23:23 -0500

    made plan for implementing pagination

--- react-app/src/components/Search/SearchResults.js
+++ react-app/src/components/Search/SearchResults.js
@@ -6,12 +6,49 @@ import UserList from '../UserList';
 
 import './search.css'
 
+/*
+    PAGINATION:
+    Things we need to track:
+        ++ current page
+            tells us the needed offset for search query
+        ++ whether there are more results after current 20 found (nextpage state/variable?)
+             tells us if we need link to next pages?
+                this method will be faster, tho less convenient to user?
+             alternatively... predetermine number of pages by dividing num results by limit?
+                larger query required, potentially problematic if many results are returned
+                could circumvent issue by doing 5-10 pages' worth of query at a time.
+
+    ++ searchPosts fx (in utils)
+        rather than use /${search}, give get request a body
+        body = search term, page num
+
+        will need setPosts and setNextPages
+    
+    ++ search route
+        if page num is not NoneType/blank, then multiply it by limit (30) to determine offset
+
+        perform query with appropriate offset (these are da posts)
+
+        if page num % 5 === 1, perform query to find out how many results there are for next 5 pages worth of content. ceiling of divided len of results by 30
+
+        create list of range(currentPage, numPages + 1)
+
+        return posts and page nums
+
+    ++ search results component
+        maybe in postUtils, add a nextPageLink fx
+            creates navLinks for the next ~5 pages from the page nums returned in the search route
+        
+        
+
+*/
+
 export default function SearchResults() {
    const [users, setUsers] = useState({});
    const [posts, setPosts] = useState({});
-   const { search } = useLocation();
+   const location = useLocation();
 
-   const query = new URLSearchParams(search).get('q');
+   const query = new URLSearchParams(location.search).get('q');
 
    useEffect(() => {
       searchUsers(query, setUsers);
