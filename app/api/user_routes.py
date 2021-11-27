68fe48bc8be3a7e2e8691eb6ef0611a7b65104a8
michellekontoff <mlkontoff@gmail.com>
2021-11-27 18:22:49 -0500

    made plan for editing profile bio/languages

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -32,3 +32,31 @@ def search_users(search):
     users = User.query.filter(User.username.ilike(f'%{search}%')).all()
 
     return { 'users': [user.to_dict() for user in users] }
+
+'''
+            ****Profile CRUD****
+    add/edit routes for bio and languages separately
+    Need forms:
+        Bio: just a textarea input
+
+        languages: speaks & learning are a select field for languageId
+
+        needs userId for both
+    
+    bio edit is straightforward, just add/replace old bio
+
+    languages:
+        to add: validation:
+            need to grab list of current languages. if language is already in list, throw error.
+            if no error, make new speakers/learners
+        can't edit? probably just delete, then re-add
+
+    FRONTEND:
+        edit profile -> two forms
+        top is edit bio
+        then
+        language shows rows of languages spoken, with delete option next to it.
+        below that, is a row that is the form to add a new language.
+        after submission, language is added to listed with delete option and new form for new language is rendered at bottom
+
+'''
