b304ee379e0c8cd016b308e9c5a3f3f2cefbe434
michellekontoff <mlkontoff@gmail.com>
2021-11-28 15:34:51 -0500

    added message if user doesnt exist

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -19,14 +19,14 @@ export default function Profile() {
          const data = await res.json();
          setUser(data);
       } else {
-         return 'Something went wrong.';
+         return 'No such user exists.';
       }
    }
 
-   async function getPosts(userId) {
-       if (userId) {
+   async function getPosts() {
+       if (user.id) {
 
-           const res = await fetch(`/api/users/${userId}/posts`);
+           const res = await fetch(`/api/users/${user.id}/posts`);
            if (res.ok) {
               const data = await res.json();
               if (data.posts.length) {
@@ -36,39 +36,38 @@ export default function Profile() {
                   noPosts();
               }
            } else {
-              return 'Something went wrong.';
+              return 'Something went wrong fetching posts.';
            }
        }
    }
 
    function noPosts() {
        console.log(currentUser.id, user.id)
-      if (currentUser.id == user.id) {
+      if (currentUser.id === user.id) {
          setPosts(
-            <p className='no-posts' align='center'>
+            <p className='not-found' align='center'>
                You don't have any posts yet. Why don't you try <Link to='/posts/new' className='first-post'>
                   writing one
                </Link>?
             </p>
          );
       } else {
-         setPosts(<p className='no-posts' align='center'>This user hasn't made any posts yet.</p>);
+         setPosts(<p className='not-found' align='center'>This user hasn't made any posts yet.</p>);
       }
    }
 
    useEffect(() => {
       const userId = params.userId;
-    //   setProfileId(parseInt(userId));
       getUser(userId);
    }, [params.userId]);
 
    useEffect(() => {
-    getPosts(user.id);
+      getPosts();
    }, [user])
 
    return (
       <>
-         {user.username && (
+         {user.username ? (
             <div className='posts-latest content'>
                <h1>{`${user.username}'s Posts`}</h1>
                {posts.length > 0 ? (
@@ -77,7 +76,9 @@ export default function Profile() {
                   <>{posts}</>
                )}
             </div>
-         )}
+         ) : <div className='posts-latest content'>
+                <p className='not-found' align ='center'>No such user exists.</p>
+                </div>}
       </>
    );
 }
